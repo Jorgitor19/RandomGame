4c5dc880c9667e96baa05364a7c10f5e9b33a3e3
Jorgitor19 <jorgitorh19@gmail.com>
2023-12-20 18:45:38 +0100

    Añado funcion de verificar numero

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ let textoresultado = document.querySelector('#resultado')
 //Declaramos una variable que contenga el div del HTML que tendrá el texto de estadísticas
 let textoestadisticas = document.querySelector('#estadisticas')
 //Declaramos una variable que contenga la obtención del valor de "exitosos" almacenado en el localStorage, agregamos un "or" para que se ponga como valor "0" si no existe aún dicha clave.
-let valorexitosos = localStorage.getItem('exitosos') || 0; 
+let valorexitosos = localStorage.getItem('exitosos') || 0;
 //Declaramos una variable que contenga la obtención del valor de "fallidos" almacenado en el localStorage, agregamos un "or" para que se ponga como valor "0" si no existe aún dicha clave.
 let valorfallidos = localStorage.getItem('fallidos') || 0;
 
@@ -25,11 +25,11 @@ let valorfallidos = localStorage.getItem('fallidos') || 0;
 
 
 //Función generadora del número
-function generarNumeroAleatorio() { 
-//Declaramos una función que generará el número aleatorio.
-numeroalmacenado = Math.floor(Math.random() * 10); //
-//Le añade como valor a la variable que va a contener dicho número un random como número entero.
-console.info(numeroalmacenado);
+function generarNumeroAleatorio() {
+    //Declaramos una función que generará el número aleatorio.
+    numeroalmacenado = Math.floor(Math.random() * 10); //
+    //Le añade como valor a la variable que va a contener dicho número un random como número entero.
+    console.info(numeroalmacenado);
 }
 //Función verificadora de número
 botonparacomprobar.addEventListener('click', verificarNumero)
@@ -37,16 +37,20 @@ botonparacomprobar.addEventListener('click', verificarNumero)
 
 
 //Declaramos una función que comprobará el número que agreguemos.
-function verificarNumero() {}
+function verificarNumero() { }
 
 //Declaramos una variable local que coge el valor del input donde el usuario va a escribir su número.
-let numerojugador = document.querySelector('#numerojugador').value;
+let numerojugador = document.querySelector('#numeroUsuario').value;
 //Ese valor lo transformamos en un número entero (parseInt).
 numerojugador = parseInt(numerojugador);
 //Si el número del usuario es estrictamente igual que el número almacenado en la variable que contiene el número aleatorio:
-//Aumentamos el valor de la variable que contiene el número de éxitos.
-//Guardamos el valor en el localStorage con la clave correspondiente al almacenamiento de éxitos.
+if (numerojugador === numeroalmacenado) {
+    //Aumentamos el valor de la variable que contiene el número de éxitos.
+    valorexitosos++;
+    //Guardamos el valor en el localStorage con la clave correspondiente al almacenamiento de éxitos.
+    localStorage.setItem('exitosos', valorexitosos);
 //Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de resultado y agregamos el contenido que queramos. Por ejemplo "¡Muy bien!".
+textoResultado.innerHTML = "<div>Muy bien amigo! El número era <b>" + numeroalmacenado + "</b></div>";
 //Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
 //Llamamos a la función generadora de números para generar un nuevo número aleatorio.
 //Si no:
