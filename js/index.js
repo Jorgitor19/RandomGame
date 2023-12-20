80cc577db31f246ef1e91389fde157f6f9e1cf24
Jorgitor19 <jorgitorh19@gmail.com>
2023-12-20 18:50:21 +0100

    Funcion completa

--- js/index.js
+++ js/index.js
@@ -25,39 +25,51 @@ let valorfallidos = localStorage.getItem('fallidos') || 0;
 
 
 //Función generadora del número
-function generarNumeroAleatorio() {
+function generarnumerorandom() {
     //Declaramos una función que generará el número aleatorio.
     numeroalmacenado = Math.floor(Math.random() * 10); //
     //Le añade como valor a la variable que va a contener dicho número un random como número entero.
     console.info(numeroalmacenado);
 }
 //Función verificadora de número
-botonparacomprobar.addEventListener('click', verificarNumero)
+botonparacomprobar.addEventListener('click', verificarnumero)
 
 
 
 //Declaramos una función que comprobará el número que agreguemos.
-function verificarNumero() { }
+function verificarnumero() {
 
-//Declaramos una variable local que coge el valor del input donde el usuario va a escribir su número.
-let numerojugador = document.querySelector('#numeroUsuario').value;
-//Ese valor lo transformamos en un número entero (parseInt).
-numerojugador = parseInt(numerojugador);
-//Si el número del usuario es estrictamente igual que el número almacenado en la variable que contiene el número aleatorio:
-if (numerojugador === numeroalmacenado) {
-    //Aumentamos el valor de la variable que contiene el número de éxitos.
-    valorexitosos++;
-    //Guardamos el valor en el localStorage con la clave correspondiente al almacenamiento de éxitos.
-    localStorage.setItem('exitosos', valorexitosos);
-//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de resultado y agregamos el contenido que queramos. Por ejemplo "¡Muy bien!".
-textoResultado.innerHTML = "<div>Muy bien amigo! El número era <b>" + numeroalmacenado + "</b></div>";
-//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
-//Llamamos a la función generadora de números para generar un nuevo número aleatorio.
-//Si no:
-//Aumentamos el valor de la variable que contiene el número de fallidos.
-//Guardamos el valor en el localStorage con la clave correspondiente al almacenamiento de fallidos.
-//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de resultado y agregamos el contenido que queramos. Por ejemplo "Fallaste".
-//Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
+    //Declaramos una variable local que coge el valor del input donde el usuario va a escribir su número.
+    let numerojugador = document.querySelector('#numeroUsuario').value;
+    //Ese valor lo transformamos en un número entero (parseInt).
+    numerojugador = parseInt(numerojugador);
+    //Si el número del usuario es estrictamente igual que el número almacenado en la variable que contiene el número aleatorio:
+    if (numerojugador === numeroalmacenado) {
+        //Aumentamos el valor de la variable que contiene el número de éxitos.
+        valorexitosos++;
+        //Guardamos el valor en el localStorage con la clave correspondiente al almacenamiento de éxitos.
+        localStorage.setItem('exitosos', valorexitosos);
+        //Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de resultado y agregamos el contenido que queramos. Por ejemplo "¡Muy bien!".
+        textoresultado.innerHTML = "<div>Muy bien amigo! El número era <b>" + numeroalmacenado + "</b></div>";
+        //Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
+        textoestadisticas.innerHTML = `Intentos buenos: ${valorexitosos}, Intentos Malos: ${valorfallidos}`;
+        //Llamamos a la función generadora de números para generar un nuevo número aleatorio.
+        generarnumerorandom();
+        //Si no:
+    } else {
+        //Aumentamos el valor de la variable que contiene el número de fallidos.
+        valorfallidos++;
+        //Guardamos el valor en el localStorage con la clave correspondiente al almacenamiento de fallidos.
+        localStorage.setItem('fallidos', valorexitosos);
+        //Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de resultado y agregamos el contenido que queramos. Por ejemplo "Fallaste".
+        textoresultado.innerHTML = "<div> Has fallado, intentalo de nuevo.</div>";
+        //Modificamos el HTML interno del objeto que contiene el div del HTML para el texto de estadísticas para actualizar los valores de éxito y de fallidos.
+        textoestadisticas.innerHTML = `Exitosos: ${valorfallidos}, Fallidos: ${intentosFallidos}`;
+    }
+}
+
+
+
 //Función limpiadora de datos
 //Declaramos una función que se encargará de limpiar los datos del localStorage
 //Usamos el método de localStorage para limpiar los datos.
